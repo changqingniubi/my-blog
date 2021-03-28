a04b8f283cb39a09caf2538c512cc9a156a9e4b1
changqing <qing.chang@eeoa.com>
2021-03-28 22:26:29 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+// 在.vuepress 创建config.js 文件 
+// 在config.js 添加配置信息
+module.exports = {
+  title: 'jiushang\'s blog',
+  description: '描述',
+  head: [ // 注入到当前页面的 <head> 中的标签
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/logo.png' }],
+  ],
+  serviceWorker: true, // 是否开启 PWA
+  base: '/docs/', // 部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块是否显示行号
+  },
+  themeConfig: {
+    // 导航栏配置
+    nav:[
+      {text: 'Home', link: '/' },
+      {text: 'Learn', link: '/learn/' },
+      {text: 'Profile', link: '/profile/' },
+      {text: 'vue', link: '/vue/' },
+      {text: 'react', link: '/react/' },
+      {text: '风花雪月', link: '/风花雪月/' }
+    ],
+    version: '0.11.0-beta'
+  }
+}
